57b06ffc4bf1448d4c780bd8780f61e55f59260c
Isaac Mutugi <isaacmutugi06@gmail.com>
2022-09-26 07:00:28 +0300

    first commit

---
+++ 5-utils.js
@@ -0,0 +1,6 @@
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+// exporting single value
+module.exports = sayHi
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,9 @@
+// direct exports
+module.exports.items = ['item1', 'item2']
+
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person
+
